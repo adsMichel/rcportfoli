5e22aeadd72178163ebefae9cc5cc2f19b4184a7
Michel Naoki <lojachimbinha@gmail.com>
2024-01-27 19:46:13 -0300

    section portfoio finish and testimonials

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,82 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from '../../assets/portfolio1.jpg'
+import IMG2 from '../../assets/portfolio2.jpg'
+import IMG3 from '../../assets/portfolio3.jpg'
+import IMG4 from '../../assets/portfolio4.jpg'
+import IMG5 from '../../assets/portfolio5.png'
+import IMG6 from '../../assets/portfolio6.jpg'
+
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: 'Titulo One',
+    github: 'https://github.com/adsmichel',
+    demo: 'https://dribbble.com/shots/23496935-Hyper-Charts-templates-Sales'
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: 'Titulo Two',
+    github: 'https://github.com/adsmichel',
+    demo: 'https://dribbble.com/shots/23496923-Orion-UI-kit-is-a-library-with-50-full-width-dashboard-template'
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: 'Titulo Three',
+    github: 'https://github.com/adsmichel',
+    demo: 'https://dribbble.com/shots/23441431-Eclipse-Figma-dashboard-UI-kit-for-data-design-web-apps'
+  },
+  {
+    id: 4,
+    image: IMG4,
+    title: 'Titulo Four',
+    github: 'https://github.com/adsmichel',
+    demo: 'https://dribbble.com/shots/23402855-Data-presentation-and-dashboard-templates'
+  },
+  {
+    id: 5,
+    image: IMG5,
+    title: 'Titulo Five',
+    github: 'https://github.com/adsmichel',
+    demo: 'https://dribbble.com/shots/23402778-Stunning-and-professional-looking-visualizations-for-any-project'
+  },
+  {
+    id: 6,
+    image: IMG6,
+    title: 'Titulo Six',
+    github: 'https://github.com/adsmichel',
+    demo: 'https://dribbble.com/shots/23224234-Orion-UI-kit-data-visualization-and-charts-templates-for-Figma'
+  }
+]
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>Trabalhos Recentes</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+        {
+          data.map(({id, image, title, github, demo}) => {
+            return (
+              <article key={id} className='portfolio__items'>
+                <div className="portfolio__item-image">
+                  <img src={image} alt={title} />
+                </div>
+                <h3>{title}</h3>
+                <div className="portfolio__items-cta">
+                  <a href={github} className='btn'>Github</a>
+                  <a href={demo} className='btn btn-primary' target='_blank'>Live Demo</a>
+                </div>
+              </article>
+            )
+          })
+        }
+      </div>
+    </section>
   )
 }
 
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,55 @@
 import React from 'react'
 import './testimonials.css'
+import AVTR1 from '../../assets/avatar1.jpg'
+import AVTR2 from '../../assets/avatar2.jpg'
+import AVTR3 from '../../assets/avatar3.jpg'
+import AVTR4 from '../../assets/avatar4.jpg'
 
 const Testimonials = () => {
   return (
-    <section id='testimonials'>Testimonials</section>
+    <section id='testimonials'>
+      <h5>Avaliações dos clientes</h5>
+      <h2>Avaliações</h2>
+
+      <div className='container testimonials__container'>
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={AVTR1} alt="Avatar One" />
+          </div>
+            <h5 className='client__name'>Flávia Achiever</h5>
+            <small className='client__review'>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum dignissimos tenetur aperiam eaque, repudiandae nemo sit aliquam! Dicta incidunt quidem rem dignissimos quis at inventore. Beatae similique natus consequatur autem!
+            </small>
+        </article>
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={AVTR2} alt="Avatar Two" />
+          </div>
+            <h5 className='client__name'>Eliano Mundim</h5>
+            <small className='client__review'>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum dignissimos tenetur aperiam eaque, repudiandae nemo sit aliquam! Dicta incidunt quidem rem dignissimos quis at inventore. Beatae similique natus consequatur autem!
+            </small>
+        </article>
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={AVTR3} alt="Avatar Three" />
+          </div>
+            <h5 className='client__name'>Arthur Nogueira</h5>
+            <small className='client__review'>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum dignissimos tenetur aperiam eaque, repudiandae nemo sit aliquam! Dicta incidunt quidem rem dignissimos quis at inventore. Beatae similique natus consequatur autem!
+            </small>
+        </article>
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={AVTR4} alt="Avatar Four" />
+          </div>
+            <h5 className='client__name'>Marcela Guimarães</h5>
+            <small className='client__review'>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum dignissimos tenetur aperiam eaque, repudiandae nemo sit aliquam! Dicta incidunt quidem rem dignissimos quis at inventore. Beatae similique natus consequatur autem!
+            </small>
+        </article>
+      </div>
+    </section>
   )
 }
 
